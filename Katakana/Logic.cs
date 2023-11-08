1766cf3cbef43f6339949a01ca5f613a8d8128b4
Adam Błoński <adamino2002@gmail.com>
2023-11-08 21:56:34 +0100

    Stworzenie klasy logika z funkcją wpisania ciągu znaków bitmapy do tabeli

---
+++ Katakana/Logic.cs
@@ -0,0 +1,15 @@
+﻿namespace Katakana
+{
+    public class Logic
+    {
+        private int[,] u_in = new int[64, 48];
+
+        public void WriteToColumn(int signNr, string bits) //Funkcja do wpisywania ciągu zaków mapy bitowej(bits) dla konkretnego znaku(signNr) do tabeli u_in
+        {
+            for (int i = 0; i < bits.Length; i++)
+            {
+                u_in[i,signNr] = (int)bits[i]-'0'; //Wpisanie danego elementu ciągu znaków na odpowiednią pozycję w tabeli
+            }
+        }
+    }
+}
