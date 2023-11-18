3a3f91a44c0ecf63ba25c328475d6090dcaf6c43
Adam Błoński <adamino2002@gmail.com>
2023-11-18 18:07:08 +0100

    Zmiany zrobione na ostatnich ćwiczeniach (9.11)

--- Katakana/Logic.cs
+++ Katakana/Logic.cs
@@ -2,13 +2,29 @@
 {
     public class Logic
     {
-        private int[,] u_in = new int[64, 48];
+        private int[,] A = new int[111, 46];
+
+        private int[] W = new int[6152];
+
+        private string[] bitMap = new string[46];
 
         public void WriteToColumn(int signNr, string bits) //Funkcja do wpisywania ciągu zaków mapy bitowej(bits) dla konkretnego znaku(signNr) do tabeli u_in
         {
-            for (int i = 0; i < bits.Length; i++)
+            for (int i = 0; i < bits.Length+1; i++)
             {
-                u_in[i,signNr] = (int)bits[i]-'0'; //Wpisanie danego elementu ciągu znaków na odpowiednią pozycję w tabeli
+                if (i == 0) 
+                    A[i, signNr] = 1;
+                else 
+                    A[i, signNr] = (int)bits[i-1] - '0'; //Wpisanie danego elementu ciągu znaków na odpowiednią pozycję w tabeli
+                A[65 + signNr, signNr] = 1;
+            }
+        }
+
+        public void WriteToTable(string[] strings)
+        {
+            for (int i = 0; i<A.GetLength(1); i++)
+            {
+                    this.WriteToColumn(i, strings[i]);   
             }
         }
     }
